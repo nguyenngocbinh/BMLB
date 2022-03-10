d0c17d56c973f9e29910161e43f8c7607bf29e0c
nguyenngocbinh <nguyenngocbinhneu@gmail.com>
2022-03-10 16:54:38 +0700

    add css xaringanthemer

--- 13-Database/2. Scripts/R/push_data_to_db.R
+++ 13-Database/2. Scripts/R/push_data_to_db.R
@@ -1,25 +1,21 @@
-13-Database
-================
 
-### Load packages cần thiết
+rm(list = ls())
 
-``` r
+# Load cac package can thiet
+
 rm(list = ls())
 library(DBI)
 library(odbc)
 library(tictoc)
 library(tidyverse)
-```
 
-### Kiểm tra các kết nối để sử dụng cho argument Driver trong hàm dbConnect
 
-``` r
+# Kiểm tra các kết nối để sử dụng cho argument Driver trong hàm dbConnect
 odbc::odbcListDrivers()
-```
 
-### Kết nối vào DB
+#-------------------------------------------------------------------------------
 
-``` r
+# Kết nối vào DB
 con <- dbConnect(odbc(),
                  Driver = "SQL Server",
                  Server = "VM-DC-JUMPSRV77\\IFRS9", # chu y phai dung 2 dau gach ngang
@@ -27,10 +23,9 @@ con <- dbConnect(odbc(),
                  # UID = "sa",
                  # PWD = "******",
                  Port = 1433)
-```
 
-### Đẩy dữ liệu vào DB
-
-``` r
+# Đẩy dữ liệu vào DB
 dbWriteTable(con, sql_tbl_name , df[1500001:nrow(df), ], append = TRUE)
-```
+
+
+
